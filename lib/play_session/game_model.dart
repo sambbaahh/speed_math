0ca841c264420fc4d1f1870843b96d018cf8e887
Sami Kukkonen <samikukkonen00@hotmail.com>
2023-10-22 00:02:33 +0300

    programming logic

--- lib/play_session/game_model.dart
+++ lib/play_session/game_model.dart
@@ -2,19 +2,17 @@ enum Difficulty { novice, advanced, master }
 
 class GameModel {
   final Difficulty difficulty;
-  int level = 1;
-
   late String mathExpression;
   late List<String> operators;
   late int maxNumber;
   late int rightAnswer;
-  late List<int> wrongAnswers;
+  late List<int> allAnswers;
 
   GameModel({required this.difficulty}) {
     mathExpression = "";
     operators = ["+", "-", "*"];
     maxNumber = 0;
     rightAnswer = 0;
-    wrongAnswers = [];
+    allAnswers = [];
   }
 }
--- lib/play_session/play_session_view.dart
+++ lib/play_session/play_session_view.dart
@@ -15,16 +15,15 @@ class Game extends StatefulWidget {
 class _GameState extends State<Game> with TickerProviderStateMixin {
   late AnimationController controller;
   late GameModel gameModel;
+
+  bool isDisabled = false;
   int _score = 0;
   int _level = 1;
 
   @override
   void initState() {
     super.initState();
-    gameModel = GameModel(difficulty: widget.difficulty);
-    gameModel.maxNumber = 10;
-    generateMathExpression();
-
+    initializeGame();
     controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 10),
@@ -34,13 +33,10 @@ class _GameState extends State<Game> with TickerProviderStateMixin {
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          setState(() {
-            _score += 50;
-            _level += 1;
-          });
+          checkAnswer(-1);
         }
       });
-    Future.delayed(const Duration(microseconds: 500), () {
+    Future.delayed(const Duration(milliseconds: 500), () {
       controller.forward();
     });
   }
@@ -51,41 +47,114 @@ class _GameState extends State<Game> with TickerProviderStateMixin {
     super.dispose();
   }
 
+  void initializeGame() {
+    gameModel = GameModel(difficulty: widget.difficulty);
+    gameModel.maxNumber = 10;
+    generateMathExpression();
+    generateWrongAnswers();
+    gameModel.allAnswers.shuffle();
+  }
+
   void generateMathExpression() {
-    gameModel.rightAnswer = 0;
-    while (gameModel.rightAnswer < 1) {
+    // Alustetaan alkuarvot
+    int rightAnswer = 0;
+    String mathExpression = "";
+
+    while (rightAnswer < 1) {
       var startValue = Random().nextInt(gameModel.maxNumber) + 1;
       var endValue = Random().nextInt(gameModel.maxNumber) + 1;
       var operator =
           gameModel.operators[Random().nextInt(gameModel.operators.length)];
 
       if (operator == "-" && endValue > startValue) {
-        gameModel.mathExpression = "$endValue $operator $startValue";
+        mathExpression = "$endValue $operator $startValue";
       } else {
-        gameModel.mathExpression = "$startValue $operator $endValue";
+        mathExpression = "$startValue $operator $endValue";
       }
 
-      Parser p = Parser();
-      Expression expression = p.parse(gameModel.mathExpression);
-      ContextModel cm = ContextModel();
-      gameModel.rightAnswer =
-          expression.evaluate(EvaluationType.REAL, cm).round();
+      rightAnswer = calculateCorrectAnswer(mathExpression);
+    }
+    gameModel.mathExpression = mathExpression;
+    gameModel.rightAnswer = rightAnswer;
+    gameModel.allAnswers.add(rightAnswer);
+  }
+
+  int calculateCorrectAnswer(String expression) {
+    Parser p = Parser();
+    Expression parsedExpression = p.parse(expression);
+    ContextModel cm = ContextModel();
+
+    return parsedExpression.evaluate(EvaluationType.REAL, cm).round();
+  }
+
+  void generateWrongAnswers() {
+    double rndFactor = 0;
+    if (gameModel.rightAnswer < 2) {
+      rndFactor = 4;
+    } else if (gameModel.rightAnswer < 3) {
+      rndFactor = 2;
+    } else if (gameModel.rightAnswer < 5) {
+      rndFactor = 1;
+    } else if (gameModel.rightAnswer < 10) {
+      rndFactor = 0.5;
+    } else if (gameModel.rightAnswer < 20) {
+      rndFactor = 0.25;
+    } else {
+      rndFactor = 0.2;
     }
 
-    for (var i = 0; i < 3; i++) {
-      int randomWrongAnswer =
-          Random().nextInt((gameModel.rightAnswer * 1.2).round()) +
-              (gameModel.rightAnswer * 0.8).round();
+    print(gameModel.rightAnswer);
 
-      gameModel.wrongAnswers.add(randomWrongAnswer);
+    while (gameModel.allAnswers.length < 4) {
+      int minValue = (gameModel.rightAnswer * (1 - rndFactor)).round();
+      int maxValue = (gameModel.rightAnswer * (1 + rndFactor)).round();
+
+      int randomWrongAnswer = Random().nextInt(maxValue - minValue) + minValue;
+
+      if (!gameModel.allAnswers.contains(randomWrongAnswer) &&
+          randomWrongAnswer != gameModel.rightAnswer &&
+          randomWrongAnswer > 0) {
+        gameModel.allAnswers.add(randomWrongAnswer);
+      }
     }
+  }
+
+  void checkAnswer(int i) {
+    isDisabled = true;
+    controller.stop();
+
+    if (i == -1) {
+      setState(() {
+        _score -= 50;
+        _level++;
+      });
+    } else if (gameModel.allAnswers[i] == gameModel.rightAnswer) {
+      setState(() {
+        _score += (100 * (1 - controller.value)).round();
+        _level++;
+      });
+    } else {
+      setState(() {
+        _score -= 50 + (100 * controller.value).round();
+        _level++;
+      });
+    }
+
+    Future.delayed(const Duration(milliseconds: 500), () {
+      controller.reset();
+    });
+    Future.delayed(const Duration(milliseconds: 500), () {
+      initializeGame();
+      isDisabled = false;
+      controller.forward();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Total score: ${_score}"),
+          title: Text("Total score: $_score"),
           bottom: PreferredSize(
             preferredSize: const Size.fromHeight(6.0),
             child: LinearProgressIndicator(
@@ -99,28 +168,37 @@ class _GameState extends State<Game> with TickerProviderStateMixin {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Level ${(_level)}"),
-              Text(gameModel.mathExpression),
-              Text(gameModel.rightAnswer.toString()),
-              Text(gameModel.wrongAnswers.toString()),
-              ElevatedButton(
-                onPressed: () {
-                  controller.stop(canceled: true);
-                },
-                child: const Text("Stop"),
+              Text(
+                "Level ${(_level)}",
+                style: const TextStyle(fontSize: 16),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.forward();
-                },
-                child: const Text("Start"),
+              const Padding(
+                padding: EdgeInsets.all(10),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.reset();
-                },
-                child: const Text("Reset"),
+              Text(
+                gameModel.mathExpression,
+                style: const TextStyle(fontSize: 30),
               ),
+              const Padding(
+                padding: EdgeInsets.all(80),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Kaksi saraketta rivillä
+                  ),
+                  itemCount: gameModel.allAnswers.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return ElevatedButton(
+                      onPressed: () => isDisabled ? null : checkAnswer(i),
+                      child: Text(
+                        gameModel.allAnswers[i].toString(),
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    );
+                  },
+                ),
+              )
             ],
           ),
         ));
